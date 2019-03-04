228845880a984b332507bdd813df3fb1b823a390
sbalnojan <SvenBalnojan@gmail.com>
2019-03-04 20:05:16 +0100

    refactor into functions, local vars

--- DClock/DClock.sh
+++ DClock/DClock.sh
@@ -8,26 +8,6 @@
 
 # Dependencies:
 
-if [ -z "$1" ]
-  then
-    . ~/.dc_config.cfg
-else
-    . .dc_config.cfg
-fi
-
-
-#DEATH_DATE_STR="2020-11-07"
-DEATH_DATE=$(date -jf "%Y-%m-%d" $DEATH_DATE_STR)
-DEATH_SEC=$(date -jf "%Y-%m-%d" ${DEATH_DATE_STR} +"%s")
-
-CURR_DATE=$(date)
-CURR_SEC=$(date +"%s")
-
-DIFF=$(((DEATH_SEC - CURR_SEC)/(86400)))
-PROJ=$((DIFF/(365*4)))
-YEARS=$((DIFF/365))
-DAYS_THIS_YEAR=$((DIFF-YEARS*365))
-
 # prints the progress of the current quarter
 #
 # Examples:
@@ -35,7 +15,7 @@ DAYS_THIS_YEAR=$((DIFF-YEARS*365))
 #   Returns: 22%
 #
 function print_quarter_prog {
-  DAYS_THIS_YEAR=$1
+  local DAYS_THIS_YEAR=$1
 
   if [[ "$DAYS_THIS_YEAR" -gt "270" ]]
     then
@@ -51,22 +31,44 @@ function print_quarter_prog {
   fi
 }
 
-if [[ QUARTER_PROGRESS ]]
-  then
-    ADD_THIS="($( print_quarter_prog $DAYS_THIS_YEAR ))"
-    ADD_THAT="$( print_quarter_prog $DAYS_THIS_YEAR ) left of quarter"
-fi
+function main {
+  if [ -z "$1" ]
+    then
+      . ~/.dc_config.cfg
+  else
+      . .dc_config.cfg
+  fi
 
-echo "${DIFF} days ${ADD_THIS}"
-echo "---"
-if [[ -n $ADD_THAT ]]; then
-  echo $ADD_THAT
-fi
+  DEATH_DATE=$(date -jf "%Y-%m-%d" $DEATH_DATE_STR)
+  DEATH_SEC=$(date -jf "%Y-%m-%d" ${DEATH_DATE_STR} +"%s")
 
-echo "${PROJ} projects"
+  CURR_DATE=$(date)
+  CURR_SEC=$(date +"%s")
 
-if [ -n "$BOOKS_A_YEAR" ]
-  then
-    BK=$((BOOKS_A_YEAR * DIFF/365))
-    echo "${BK} books"
-fi
+  DIFF=$(((DEATH_SEC - CURR_SEC)/(86400)))
+  PROJ=$((DIFF/(365*4)))
+  YEARS=$((DIFF/365))
+  DAYS_THIS_YEAR=$((DIFF-YEARS*365))
+
+  if [[ QUARTER_PROGRESS ]]
+    then
+      ADD_THIS="($( print_quarter_prog $DAYS_THIS_YEAR ))"
+      ADD_THAT="$( print_quarter_prog $DAYS_THIS_YEAR ) left of quarter"
+  fi
+
+  echo "${DIFF} days ${ADD_THIS}"
+  echo "---"
+  if [[ -n $ADD_THAT ]]; then
+    echo $ADD_THAT
+  fi
+
+  echo "${PROJ} projects"
+
+  if [ -n "$BOOKS_A_YEAR" ]
+    then
+      BK=$((BOOKS_A_YEAR * DIFF/365))
+      echo "${BK} books"
+  fi
+}
+
+main "$@"
